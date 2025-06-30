c17aa1eb26ee7f66cb5e00d63878010586ddb29f
Leandro <leandrotomasetto@gmail.com>
2025-06-30 10:33:28 -0300

    configurando django-summernote

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Tag
+from blog.models import Category, Page, Post, Tag
 
 
 @admin.register(Tag)
@@ -13,3 +13,52 @@ class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('name',),
     }
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug',
+    list_display_links = 'name',
+    search_fields = 'id', 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('name',),
+    }
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'content',
+    list_per_page = 50
+    list_filter = 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published',  'created_by',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
+    list_per_page = 50
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
+    autocomplete_fields = 'tags', 'category',
+
+    def sava_model(self, request, obj, form, change):
+        if change:
+            obj.update_by = request.user
+        else:
+            obj.created_by = request.user
+        obj.save()
